9f8f560e8ae28cab3f76a9a2e1b75af12d16238a
Gabriel-VitorS <gabrielvitordasilva@gmail.com>
2022-09-09 13:55:51 -0300

    Melhoria na validação de Users

--- app/Http/Controllers/Api/Users.php
+++ app/Http/Controllers/Api/Users.php
@@ -16,33 +16,66 @@ class Users extends Controller
         return response()->json(['Message' => 'Sucess', 'Data' => $request->user()], 200);
     }
 
-
     public function login(Request $request){
 
-        if($request->email == '' || $request->email == null || $request->password == '' || $request->password == null)
-            return response()->json(['Message' => "Invalid parameters"], 400);
+        $validator = Validator::make(
+            [
+                'email' => $request->email,     
+                'password' => $request->password,          
+            ],
+            [
+                'email' => 'required',
+                'password' => 'required',
+            ],[
+                'email.required' => 'Please enter a email',
+                'password.required' => 'Please enter a password',
+            ]);
 
+        if($validator->fails())
+            return response()->json(['Message' => $validator->messages()], 400); 
 
-        $credentials = $request->only('email', 'password');
+        try{
+            $credentials = $request->only('email', 'password');
 
-        if(Auth::attempt($credentials)){
-            $user = Auth::user();
-            $token = $user->createToken("JWT");
+            if(Auth::attempt($credentials)){
+                $user = Auth::user();
+                $token = $user->createToken("JWT");
 
-            return response()->json(['Message' => 'Sucess', 'Token' => $token->plainTextToken],200);
+                return response()->json(['Message' => 'Sucess', 'Token' => $token->plainTextToken],200);
+            }
+
+            return response()->json(['Message' => "Invalid user"], 400);
+
+        }catch(\Exception $e){
+            return response()->json(['Message' => 'Server error. Please contact the suporte'], 500);
         }
 
-
-        return response()->json(['Message' => "Invalid user"], 401);
+        
         
 
     }
 
     public function store(Request $request){
 
-        $user = new User();
+        $validator = Validator::make(
+            [
+                'email' => $request->email,     
+                'password' => $request->password,          
+            ],
+            [
+                'email' => 'required',
+                'password' => 'required',
+            ],[
+                'email.required' => 'Please enter a email',
+                'password.required' => 'Please enter a password',
+            ]);
+
+        if($validator->fails())
+            return response()->json(['Message' => $validator->messages()], 400); 
 
         try{
+            $user = new User();
+
             $user->name = $request->name;
             $user->email = $request->email;
             $user->password = Hash::make($request->password);
@@ -51,7 +84,7 @@ class Users extends Controller
             return response()->json(['Message' => "Sucess", "Id" => $user->id ] , 200);
 
         }catch(\Exception $e){
-            return response()->json(['Message' => $e ] , 400);
+            return response()->json(['Message' => 'Server error. Please contact the suporte'], 500);
         }
         
 
@@ -121,8 +154,7 @@ class Users extends Controller
 
             return response()->json(['Message' => 'Sucess'], 500);
         }catch(\Exception $e){
-            return response($e);
-            // return response()->json(['Message' => 'Server error. Please contact the suporte'], 500);
+            return response()->json(['Message' => 'Server error. Please contact the suporte'], 500);
         }
         
 
